712afe7264f97a5048a535003e4e69d5571dc752
AryaaSatya <yudistiraarya819@gmail.com>
2024-09-04 08:45:46 +0700

    Add files via upload

---
+++ Percabangan/IfThenElse/src/main/java/com/mycompany/ifthenelse/IfThenElse.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ifthenelse;
+
+/**
+ *
+ * @author VICTUS
+ */
+public class IfThenElse {
+
+    public static void main(String[] args) {
+        boolean isOn = false;
+    
+        if (isOn) {
+            System.out.println("Menyalakan Proyektor LED");
+        } else {
+            System.out.println("Kondisi Tidak Terpenuhi....");
+        }
+    }
+}
---
+++ Percabangan/IfThenElseThenElse/src/main/java/com/mycompany/ifthenelsethenelse/IfThenElseThenElse.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ifthenelsethenelse;
+
+/**
+ *
+ * @author VICTUS
+ */
+public class IfThenElseThenElse {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
